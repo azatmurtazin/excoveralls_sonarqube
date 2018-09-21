10b85127dc68a8337ab856cea0403e409b391dd8
a.murtazin <a.murtazin@fun-box.ru>
2018-09-21 11:27:49 +0400

    initial commit

---
+++ lib/excoveralls_sonarqube.ex
@@ -0,0 +1,18 @@
+defmodule ExcoverallsSonarqube do
+  @moduledoc """
+  Documentation for ExcoverallsSonarqube.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExcoverallsSonarqube.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/excoveralls_sonarqube_test.exs
@@ -0,0 +1,8 @@
+defmodule ExcoverallsSonarqubeTest do
+  use ExUnit.Case
+  doctest ExcoverallsSonarqube
+
+  test "greets the world" do
+    assert ExcoverallsSonarqube.hello() == :world
+  end
+end
