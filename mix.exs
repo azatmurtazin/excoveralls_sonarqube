027b1cde7795b8ba0126db22bd13d04dc6e7d147
a.murtazin <a.murtazin@fun-box.ru>
2018-10-25 11:59:49 +0400

    updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExcoverallsSonarqube.MixProject do
   def project do
     [
       app: :excoveralls_sonarqube,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
